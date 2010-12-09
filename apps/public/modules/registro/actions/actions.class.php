2ff47ca1c896f8a2121c429ac5e315cc4381b79f
Ragnarok <ragnarok@docksud.com.ar>
2010-12-09 00:44:41 -0300

    tostring para registro devuelve apellido, nombre

--- apps/public/modules/registro/actions/actions.class.php
+++ apps/public/modules/registro/actions/actions.class.php
@@ -13,4 +13,43 @@ require_once dirname(__FILE__).'/../lib/registroGeneratorHelper.class.php';
  */
 class registroActions extends autoRegistroActions
 {
+  public function executeListAsistencia(sfWebRequest $request)
+  {
+    $r = Doctrine::getTable('Registro')->find($request->getParameter('id'));
+    $this->forward404Unless($r);
+    $r->asistencia = true;
+    $r->save();
+    $this->getUser()->setFlash('notice','Se ha marcado como asistente a ' . $r);
+    $this->redirect('registro/index');
+  }
+  public function executeListAndroid(sfWebRequest $request)
+  {
+    $r = Doctrine::getTable('Registro')->find($request->getParameter('id'));
+    $this->forward404Unless($r);
+    $r->taller_android = true;
+    $r->save();
+    $this->getUser()->setFlash('notice','Se ha anotado al Taller Android a ' . $r);
+    $this->redirect('registro/index');
+  }
+  
+  public function executeListMoodle(sfWebRequest $request)
+  {
+    $r = Doctrine::getTable('Registro')->find($request->getParameter('id'));
+    $this->forward404Unless($r);
+    $r->taller_moodle = true;
+    $r->save();
+    $this->getUser()->setFlash('notice','Se ha anotado al Taller Moodle a ' . $r);
+    $this->redirect('registro/index');
+  }
+  public function executeListInstall(sfWebRequest $request)
+  {
+    $r = Doctrine::getTable('Registro')->find($request->getParameter('id'));
+    $this->forward404Unless($r);
+    $r->install_fest = true;
+    $r->save();
+    $this->getUser()->setFlash('notice','Se ha anotado al Install Fest a ' . $r);
+    $this->redirect('registro/index');
+  }
+
+  
 }
